89928516277ec3a3b7a20d49e56a45cca826fc1c
Erik Nyquist <eknyquist@gmail.com>
2016-03-25 19:00:20 +0000

    test_regexvm_err: add more tests
    Finish adding tests for all RVM_* return codes

--- tests/src/run_tests.c
+++ tests/src/run_tests.c
@@ -11,8 +11,8 @@ int main (void)
 
     ret = 0;
     ret += test_regexvm_compile();
-    ret += test_regexvm_err();
     ret += test_regexvm();
+    ret += test_regexvm_err();
 
     msg = (ret == 0) ? "passed" : "failed";
     printf("Test run %s\n", msg);
--- tests/src/test_regexvm_err.c
+++ tests/src/test_regexvm_err.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "regexvm.h"
 
-#define NUMTESTS             11
+#define NUMTESTS             22
 
 typedef struct errtest errtest_t;
 
@@ -66,11 +66,69 @@ static const errtest_t test_err_badclass_11 = {
     .err = RVM_BADCLASS
 };
 
+static const errtest_t test_err_badparen_12 = {
+    .rgx = ")",
+    .err = RVM_BADPAREN
+};
+
+static const errtest_t test_err_badparen_13 = {
+    .rgx = "ab)",
+    .err = RVM_BADPAREN
+};
+
+static const errtest_t test_err_badparen_14 = {
+    .rgx = "(a+b?(d)*))",
+    .err = RVM_BADPAREN
+};
+
+static const errtest_t test_err_badparen_15 = {
+    .rgx = "(a?(wde)*dd)+dsdssd)f",
+    .err = RVM_BADPAREN
+};
+
+static const errtest_t test_err_eparen_16 = {
+    .rgx = "(a",
+    .err = RVM_EPAREN
+};
+
+static const errtest_t test_err_eparen_17 = {
+    .rgx = "(a*(bb+(eiof)*dsds)alpalp",
+    .err = RVM_EPAREN
+};
+
+static const errtest_t test_err_eparen_18 = {
+    .rgx = "(a*(bb+(eiof)*dsds)alpalp",
+    .err = RVM_EPAREN
+};
+
+static const errtest_t test_err_eparen_19 = {
+    .rgx = "((aa(bb)*)?",
+    .err = RVM_EPAREN
+};
+
+static const errtest_t test_err_eclass_20 = {
+    .rgx = "[A-Z",
+    .err = RVM_ECLASS
+};
+
+static const errtest_t test_err_eclass_21 = {
+    .rgx = "aa(bb(c[def\\*)*)+ss",
+    .err = RVM_ECLASS
+};
+
+static const errtest_t test_err_etrail_22 = {
+    .rgx = "ss\\",
+    .err = RVM_ETRAIL
+};
+
 static const errtest_t *tests[NUMTESTS] = {
     &test_err_badop_1, &test_err_badop_2, &test_err_badop_3, &test_err_badop_4,
     &test_err_badop_5, &test_err_badop_6, &test_err_badclass_7,
     &test_err_badclass_8, &test_err_badclass_9, &test_err_badclass_10,
-    &test_err_badclass_11
+    &test_err_badclass_11, &test_err_badparen_12, &test_err_badparen_13,
+    &test_err_badparen_14, &test_err_badparen_15, &test_err_eparen_16,
+    &test_err_eparen_17, &test_err_eparen_18, &test_err_eparen_19,
+    &test_err_eclass_20, &test_err_eclass_21, &test_err_etrail_22
 };
 
 int test_regexvm_err (void)
@@ -94,7 +152,7 @@ int test_regexvm_err (void)
                 regexvm_free(&compiled);
 
         printf("%s: test %d %s\n", __func__, i + 1,
-            (ret) ? "failed" : "passed");
+            (err == 0) ? "failed" : "passed");
     }
 
     return ret;
