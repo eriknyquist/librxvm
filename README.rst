ceef8f0d54291d1158ae6094ea3691370a757324
Erik Nyquist <eknyquist@gmail.com>
2016-03-27 17:04:46 +0100

    Add API examples

--- README.rst
+++ README.rst
@@ -16,3 +16,57 @@ To install, do the usual stuff:
     ./configure
     make
     sudo make install
+
+Usage
+^^^^^
+
+See sample code in the *examples* directory. The examples compile into simple
+command-line programs, so you can look at the source to see how the library is
+used, and then build them to try out some test expressions from your shell.
+
+|
+
+Building the examples
+---------------------
+
+**NOTE: You must install libregexvm before building the examples**
+::
+
+    cd examples
+    make
+
+Using the examples
+------------------
+::
+
+   $> ./regexvm_iter
+
+     Usage: ./regexvm_iter <regex> <input>
+
+   $>./regexvm_iter "regex(vm)*" "UtrUygHIuregexvmvmvmllTRDrHIOIP"
+
+     Match!
+     Found matching substring:
+     regexvmvmvm
+
+   $> ./regexvm_iter "regex(vm)*" "UtrUygHIuregexllTRDrHIOIP"
+
+     Match!
+     Found matching substring:
+     regex
+
+   $> ./regexvm_iter "regex(vm)*" "UtrUygHIuregellTRDrHIOIP"
+
+     No match.
+
+   $>
+
+To link your own code with libregexvm, compile with
+::
+
+    -I/usr/local/include/libregexvm
+
+and link with
+::
+
+    -L/usr/local/lib -lregexvm
---
+++ examples/regexvm_iter.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "regexvm.h"
+
+static void print_substring (char *start, char  *end);
+static void regexvm_print_err (int err);
+
+int main (int argc, char *argv[])
+{
+    char *start;
+    char *end;
+    int ret;
+    regexvm_t compiled;
+
+    ret = 0;
+    if (argc != 3) {
+        printf("Usage: %s <regex> <input>\n", argv[0]);
+        exit(1);
+    }
+
+    /* Compile the expression */
+    if ((ret = regexvm_compile(&compiled, argv[1])) < 0) {
+        regexvm_print_err(ret);
+        exit(ret);
+    }
+
+    /* print the compiled expression (VM instructions) */
+    regexvm_print(&compiled);
+
+    /* Check if input string matches expression */
+    if (regexvm_iter(&compiled, argv[2], &start, &end)) {
+        printf("Match!\n");
+        printf("Found matching substring:\n");
+        print_substring(start, end);
+    } else {
+        printf("No match.\n");
+        ret = 1;
+    }
+
+    regexvm_free(&compiled);
+    return ret;
+}
+
+void print_substring (char *start, char *end)
+{
+    while (start != end) {
+        printf("%c", *start);
+        ++start;
+    }
+    printf("\n");
+}
+
+void regexvm_print_err (int err)
+{
+    const char *msg;
+
+    switch (err) {
+        case RVM_BADOP:
+            msg = "Operator used incorrectly";
+        break;
+        case RVM_BADCLASS:
+            msg = "Unexpected character class closing character";
+        break;
+        case RVM_BADPAREN:
+            msg = "Unexpected parenthesis group closing character";
+        break;
+        case RVM_EPAREN:
+            msg = "Unterminated parenthesis group";
+        break;
+        case RVM_ECLASS:
+            msg = "Unterminated character class";
+        break;
+        case RVM_ETRAIL:
+            msg = "Trailing escape character";
+        break;
+        case RVM_EMEM:
+            msg = "Failed to allocate memory";
+        break;
+        case RVM_ENEST:
+            msg = "Too many nested parenthesis groups";
+        break;
+        case RVM_ECLASSLEN:
+            msg = "Too many elements in character class";
+        break;
+        case RVM_EINVAL:
+            msg = "Unrecognised symbol";
+        break;
+        default:
+            msg = "Unrecognised error code";
+    }
+
+    printf("Error %d: %s\n", err, msg);
+}
