86e5a9d78f19fe89136446b1edb7b47bdae16354
Erik Nyquist <eknyquist@gmail.com>
2016-04-04 02:00:43 +0100

    Remove unused ENEST and ECLASSLEN errors

--- src/regexvm_err.h
+++ src/regexvm_err.h
@@ -31,8 +31,6 @@
 #define RVM_ECLASS     -5   /* Unterminated character class */
 #define RVM_ETRAIL     -6   /* Trailing escape character */
 #define RVM_EMEM       -7   /* Failed to allocate memory */
-#define RVM_ENEST      -8   /* Too many nested parenthesis groups */
-#define RVM_ECLASSLEN  -9   /* Too many elements in character class */
-#define RVM_EINVAL    -10   /* Invalid symbol */
+#define RVM_EINVAL     -8   /* Invalid symbol */
 
 #endif
