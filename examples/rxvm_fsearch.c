eriknyquist/librxvm

commit 3d47c27e453996d928e0e16ab851ec0d44551b01

Two significant VM throughput optimisations
1. Instead of exiting the VM and calling again for each failed match (when
   using rxvm_fsearch, for example, this will happen for every character
   until a match begins), simply use a goto and jump back to the start
   of vm_execute().

   In addition to avoiding the potential overhead of invoking vm_execute()
   following every non-matching character, this also allows the initial
   (costly) memset call at the beginning of vm_execute, which is unneeded in
   this case.

2. After consuming a character, the two thread tables and their
   respective lookup tables must be swapped. However, if only a single
   thread was created and it failed (again,a common case for rxvm_fsearch),
   then we can do a much simpler and quicker version of the swap.

The combined effect of these two changes appears to be a 100% increase
in throughput, at least for the following simple test case:

I generate a plain text file like so (usually test with multiple files,
ranging in size from 1MB to 5GB)-

    > cat /dev/urandom | base64 | head -c<file_size_in_bytes>

Then, I execute the rxvm_fsearch example app, searching the file for an
expression that is sufficiently complicated to ensure it wouldn't be in
the test file by chance. Before this commit, throughput on my machine
was ranging between ~50-60 million chars. per sec. Now, it's going at ~100-125
million chars. per sec.

src/rxvm.c
@@ -113,6 +113,7 @@ int rxvm_search (rxvm_t *compiled, char *input, char **start, char **end,
     tm.icase = (flags & RXVM_ICASE);
     tm.nongreedy = (flags & RXVM_NONGREEDY);
     tm.multiline = (flags & RXVM_MULTILINE);
+    tm.search = 1;
 
     ret = 0;
     if (compiled->simple) {
@@ -121,7 +122,7 @@ int rxvm_search (rxvm_t *compiled, char *input, char **start, char **end,
         if ((ret = vm_init(&tm, compiled->size)) != 0)
             goto cleanup;
 
-        while (vm_execute(&tm, compiled) && tm.match_end == 0);
+        vm_execute(&tm, compiled);
     }
 
     if (tm.match_end == 0) {
@@ -223,6 +224,7 @@ int rxvm_fsearch (rxvm_t *compiled, FILE *fp, uint64_t *match_size,
     tm.icase = (flags & RXVM_ICASE);
     tm.nongreedy = (flags & RXVM_NONGREEDY);
     tm.multiline = (flags & RXVM_MULTILINE);
+    tm.search = 1;
     charcp = &tm.chars;
     start = fpos;
 
@@ -233,7 +235,7 @@ int rxvm_fsearch (rxvm_t *compiled, FILE *fp, uint64_t *match_size,
         if ((ret = vm_init(&tm, compiled->size)) != 0)
             goto cleanup;
 
-        while (vm_execute(&tm, compiled) && tm.match_end == 0);
+        vm_execute(&tm, compiled);
     }
 
     if (tm.match_end) {

src/vm.c
@@ -89,6 +89,7 @@ int vm_execute (threads_t *tm, rxvm_t *compiled)
     tm->csize = 0;
 
     memset(tm->table_base, 0, VM_TABLE_SIZE(compiled->size));
+start:
     tm->match_start = tm->chars;
     add_thread_curr(tm, 0);
 
@@ -96,7 +97,8 @@ int vm_execute (threads_t *tm, rxvm_t *compiled)
         /* if no threads are queued for this input character,
          * then the expression cannot match, so exit */
         if (!tm->csize) {
-            return 1;
+            if (!tm->search || tm->match_end) return 1;
+            else goto start;
         }
 
         C = (*tm->getchar)(tm->getchar_data);
@@ -149,21 +151,30 @@ int vm_execute (threads_t *tm, rxvm_t *compiled)
             }
         }
 
-        /* Threads saved for the next input character
-         * are now threads for the current character.
-         * Threads for the next character are empty again.*/
-        dtemp = tm->cp;
-        tm->cp = tm->np;
-        tm->np = dtemp;
-        tm->csize = tm->nsize;
-        tm->nsize = 0;
+        if (!tm->nsize && tm->csize == 1) {
+            /* If we've used no thread slots for the next character
+             * and only one for the current character (common case when
+             * searching through a file with many non-matching characters),
+             * then we can avoid the full swap and do this shorter version */
+            tm->cp_lookup[0] = 0;
+            tm->csize = 0;
+        } else {
+            /* Full swap. Threads saved for the next input character
+             * are now threads for the current character.
+             * Threads for the next character are empty again.*/
+            dtemp = tm->cp;
+            tm->cp = tm->np;
+            tm->np = dtemp;
+            tm->csize = tm->nsize;
+            tm->nsize = 0;
 
-        /* swap lookup tables */
-        ltemp = tm->cp_lookup;
-        tm->cp_lookup = tm->np_lookup;
-        tm->np_lookup = ltemp;
+            /* Swap lookup tables */
+            ltemp = tm->cp_lookup;
+            tm->cp_lookup = tm->np_lookup;
+            tm->np_lookup = ltemp;
+            memset(tm->np_lookup, 0, compiled->size);
+        }
 
-        memset(tm->np_lookup, 0, compiled->size);
         tm->lastinput = C;
     } while (C != tm->endchar);
     return 0;

src/rxvm.h
@@ -30,6 +30,7 @@
 #define RXVM_ICASE           0x1
 #define RXVM_NONGREEDY       0x2
 #define RXVM_MULTILINE       0x4
+#define RXVM_SEARCH          0x8
 
 typedef struct rxvm_gencfg rxvm_gencfg_t;
 typedef struct rxvm rxvm_t;

src/vm.h
@@ -49,6 +49,7 @@ struct threads {
     uint8_t icase;
     uint8_t nongreedy;
     uint8_t multiline;
+    uint8_t search;
 };
 
 int vm_init (threads_t *tm, unsigned int size);

commit 7dc6ce8d7ecf38dd20dbcf75e6c2d1d84528b34a

Fix pattern reset bug introduced by 3d47c27
The changes in 3d47c27 prevents vm_execute from returning on every
mismatch. Some extra work is required to reset the match pointers in this
case, and this was overlooked in the initial commit.

examples/rxvm_fsearch.c
@@ -29,6 +29,7 @@ int main (int argc, char *argv[])
     uint64_t size;
     int i;
     int ret;
+    int flags;
     char *match;
     rxvm_t compiled;
 
@@ -50,8 +51,9 @@ int main (int argc, char *argv[])
         exit(1);
     }
 
+    flags = RXVM_MULTILINE;
     /* Find all occurrences of compiled expression in file */
-    while (rxvm_fsearch(&compiled, fp, &size, 0)) {
+    while (rxvm_fsearch(&compiled, fp, &size, flags)) {
         if ((match = get_matching_text(fp, size)) == NULL) {
             printf("Error reading matching text from file '%s'\n", argv[2]);
             exit(1);

src/rxvm.c
@@ -72,23 +72,36 @@ char getchar_str (void *data)
 
 static int simple_match (threads_t *tm, char *simple)
 {
-    char _c;
+    char c;
+    char *orig;
 
+    orig = simple;
     tm->match_start = tm->chars;
+
     while (*simple) {
-        _c = (*tm->getchar)(tm->getchar_data);
+        c = (*tm->getchar)(tm->getchar_data);
         ++tm->chars;
 
-        if (_c == tm->endchar)
+        if (c == tm->endchar)
             return 1;
 
         if (*simple == '\\')
             ++simple;
 
-        if (!char_match(tm->icase, *simple, _c))
-            return 0;
+skip_readchar:
+        if (!char_match(tm->icase, *simple, c)) {
+            if (!tm->search) return 0;
 
-        ++simple;
+            tm->match_start = tm->chars;
+
+            if (simple != orig) {
+                simple = orig;
+                --tm->match_start;
+                goto skip_readchar;
+            }
+        } else {
+            ++simple;
+        }
     }
 
     tm->match_end = tm->chars + 1;
@@ -117,7 +130,7 @@ int rxvm_search (rxvm_t *compiled, char *input, char **start, char **end,
 
     ret = 0;
     if (compiled->simple) {
-        while (!simple_match(&tm, compiled->simple));
+        simple_match(&tm, compiled->simple);
     } else {
         if ((ret = vm_init(&tm, compiled->size)) != 0)
             goto cleanup;
@@ -213,7 +226,7 @@ int rxvm_fsearch (rxvm_t *compiled, FILE *fp, uint64_t *match_size,
     char file_buffer[FBUF_SIZE];
 
     if (!compiled || !fp) return RXVM_EPARAM;
-    if ((fpos = ftell(fp)) == -1L) return RXVM_IOERR;
+    if ((fpos = ftell(fp)) < 0) return RXVM_IOERR;
 
     memset(&tm, 0, sizeof(threads_t));
     tm.getchar = getchar_file;
@@ -246,7 +259,6 @@ int rxvm_fsearch (rxvm_t *compiled, FILE *fp, uint64_t *match_size,
         if (match_size) *match_size = msize;
 
         fseek(fp, seek_size, SEEK_SET);
-        fpos = seek_size + msize;
         ret = 1;
     }
 

src/vm.c
@@ -91,7 +91,7 @@ int vm_execute (threads_t *tm, rxvm_t *compiled)
     tm->csize = 0;
 
     memset(tm->table_base, 0, VM_TABLE_SIZE(compiled->size));
-start:
+vm_start:
     tm->match_start = tm->chars;
     add_thread_curr(tm, 0);
 
@@ -100,6 +100,7 @@ start:
         C = (*tm->getchar)(tm->getchar_data);
         ++tm->chars;
 
+skip_readchar:
         /* run all the threads for this input character */
         for (t = 0; t < tm->csize; ++t) {
             ii = tm->cp[t];    /* index of current instruction */
@@ -150,10 +151,10 @@ start:
         if (C == tm->endchar) break;
 
         if (!tm->nsize) {
-            /* If we've used no thread slots for the next character
-             * and only one for the current character (common case when
-             * searching through a file with many non-matching characters),
-             * then we can avoid the full swap and do this shorter version */
+            /* If we've used no thread slots for the next character, avoid
+             * the full swap and do this shorter version. Also, if tm->search
+             * is set, we can avoid returning from vm_execute() and just jump
+             * back to the start instead */
             if (!tm->search || tm->match_end) {
                 return 1;
             }
@@ -163,7 +164,14 @@ start:
             }
 
             tm->csize = 0;
-            goto start;
+
+            if ((tm->chars - 1) != tm->match_start) {
+                tm->match_start = tm->chars - 1;
+                add_thread_curr(tm, 0);
+                goto skip_readchar;
+            }
+
+            goto vm_start;
         } else {
             /* Full swap. Threads saved for the next input character
              * are now threads for the current character.